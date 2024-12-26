3c7e1705f6611e582b1d7ddcc9c6e0d5167d90e9
HadiRezaie <Hadi.Rezaie2015@gmail.com>
2024-12-26 17:04:51 +0430

    Account controller updated.

--- src/main/java/af/mcit/accounts/controller/AccountsController.java
+++ src/main/java/af/mcit/accounts/controller/AccountsController.java
@@ -1,14 +1,33 @@
 package af.mcit.accounts.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import af.mcit.accounts.constants.AccountsConstants;
+import af.mcit.accounts.entity.dto.CustomerDto;
+import af.mcit.accounts.entity.dto.ResponseDto;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
 @RestController
+@RequestMapping(path = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
+@AllArgsConstructor
+@Validated
 public class AccountsController {
 
-    @GetMapping("/hello-world")
-    public String helloWorld() {
-        return "Hello world!";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
 }
