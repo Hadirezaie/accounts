618a2df49e3c1616b62db491d01f780ff0b4c19e
HadiRezaie <Hadi.Rezaie2015@gmail.com>
2024-12-28 14:48:48 +0430

    necessaty methods added to IAccountsService.

--- src/main/java/af/mcit/accounts/service/IAccountsService.java
+++ src/main/java/af/mcit/accounts/service/IAccountsService.java
@@ -3,10 +3,13 @@ package af.mcit.accounts.service;
 import af.mcit.accounts.service.dto.CustomerDto;
 
 public interface IAccountsService {
-    /**
-     *
-     * @param customerDto - CustomerDto Object
-     */
+
     void createAccount(CustomerDto customerDto);
+
+    CustomerDto fetchAccount(String mobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
 
 }
